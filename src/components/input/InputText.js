0f9229e7cca8354a516e90238d995bee8d122aec
Vivaldy Andhira <dididlav@gmail.com>
2020-07-15 22:08:37 +0700

    css mostly done

--- src/components/input/InputText.js
+++ src/components/input/InputText.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { Input } from "semantic-ui-react";
 
 class InputText extends Component {
   render() {
     const { disabled, onTxtChange, value } = this.props;
     return (
-      <input
+      <Input
         type="text"
+        focus
         value={value}
         disabled={disabled}
         onChange={onTxtChange}
